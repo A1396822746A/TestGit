5020f2b2ec8c083597b650c5ab599fed437fd3f8
A1396822746A <1396822746@qq.com>
2020-06-18 17:42:42 +0800

    first commit

---
+++ src/HelloWord.java
@@ -0,0 +1,11 @@
+/**
+ * 测试Git
+ * @author 谢黎明
+ * @version 1.0
+ * @date 2020/6/18 17:39
+ */
+public class HelloWord {
+  public static void main(String[] args) {
+    System.out.println("测试Git");
+  }
+}
